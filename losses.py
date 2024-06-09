616b161406ec60df09c6eaff5b75d9b84002c2b5
Nishant Dahal <nishantdahal13@gmail.com>
2024-06-09 02:02:35 +0000

    Missing libraries

--- losses.py
+++ losses.py
@@ -1,3 +1,7 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
 class mIoULoss(nn.Module):
     def __init__(self, weight=None, size_average=True, n_classes=2):
         super().__init__()
